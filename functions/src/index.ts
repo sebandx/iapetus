fb67db32da73e2cc1b906186c281ec297b093e9a
sebandx <enkai.liu.1@gmail.com>
2025-06-14 22:11:14 -0400

    dummy functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// functions/src/index.ts
 
-import {onRequest} from "firebase-functions/v2/https";
+// Import the necessary modules from the Firebase SDKs.
+// onDocumentCreated is the specific trigger we need.
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
+// The logger is used to write logs that you can view in the Google Cloud console.
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+/**
+ * This function is triggered whenever a new document is created in the
+ * 'calendarEvents' subcollection for any user.
+ *
+ * The path "/users/{userId}/calendarEvents/{eventId}" uses wildcards
+ * to match any user ID and any new event ID.
+ */
+export const onCalendarEventCreate = onDocumentCreated("/users/{userId}/calendarEvents/{eventId}", (event) => {
+  // Start logging so we can see the function ran.
+  logger.info("onCalendarEventCreate function triggered!");
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  // The 'event' object contains information about the trigger.
+  // We can get the wildcard values from the path like this:
+  const userId = event.params.userId;
+  const eventId = event.params.eventId;
+
+  logger.log(`New event [${eventId}] created for user [${userId}]`);
+
+  // The actual data of the document that was created.
+  const eventData = event.data?.data();
+
+  if (eventData) {
+    logger.log("New Event Data:", eventData);
+    // You can access specific fields like eventData.title, etc.
+  } else {
+    logger.warn("No data was found in the created document.");
+  }
+
+  // In the future, you will add logic here to create a to-do task in Firestore.
+  return;
+});
